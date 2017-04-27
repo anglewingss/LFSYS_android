9d009391a5ddf7798e036b504076d1a41b7e972e
wangbingyan90 <1418885488@qq.com>
2017-04-27 23:21:44 +0800

    search_view
    搜索的显示界面

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # LFSYS_android
 失物招领-安卓子系统
 
+最小版本4.0
+	
+API Level 15
+
+需求包
 compile 'com.android.support:design:25.0.1'
--- java/com/wangby/www/lfsys_android/Activity/SearchActivity.java
+++ java/com/wangby/www/lfsys_android/Activity/SearchActivity.java
@@ -2,6 +2,7 @@ package com.wangby.www.lfsys_android.Activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.wangby.www.lfsys_android.R;
 
@@ -9,9 +10,15 @@ import com.wangby.www.lfsys_android.R;
  * Created by 王炳炎 on 2017/4/26.
  */
 public class SearchActivity extends Activity{
+
+    EditText editText;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);}
+        setContentView(R.layout.activity_search);
+    }
+
+
+
 
 
 }
